d6a3a7282a7c5cde16e0ed23c569a706c389ac45
ser-gi-o <sergio@americansole.com>
2021-08-29 08:20:03 -0700

    trying new package for google analytics uses npm

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -88,6 +88,6 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
-    ['@vuepress/google-analytics', { 'ga': 'G-Q2NBPERYMB'}]
+    ['google-gtag', { 'ga': 'G-Q2NBPERYMB'}]
   ]
 }
