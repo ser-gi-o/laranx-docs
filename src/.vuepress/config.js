71226e6d90b078dcf61446796e16a9a0cc837e8c
ser-gi-o <sergio@americansole.com>
2021-08-29 06:33:52 -0700

    added google analytics

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -88,5 +88,9 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    '@vuepress/google-analytics',
+    {
+      'ga': 'G-Q2NBPERYMB' // UA-00000000-0
+    }
   ]
 }
